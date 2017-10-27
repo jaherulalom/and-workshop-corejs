05854371f20ef310d25377cf2e81753f406711dd
cirpo <alessandro.cinelli@gmail.com>
2017-10-27 14:02:10 +0100

    added prettier

--- 01_variables.test.js
+++ 01_variables.test.js
@@ -1,3 +1,21 @@
+function varTest() {
+  var x = 1;
+  if (true) {
+    var x = 2; // same variable!
+    console.log(x); // 2
+  }
+  console.log(x); // 2
+}
+
+function letTest() {
+  let x = 1;
+  if (true) {
+    let x = 2; // different variable
+    console.log(x); // 2
+  }
+  console.log(x); // 1
+}
+
 test('adds 1 + 2 to equal 3', () => {
   expect(1 + 2).toBe(3);
 });
