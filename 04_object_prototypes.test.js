b41be59c5e541706bd62b98dc29cb1d8bab5cdb8
louisbichard <contact@louisjohnbichard.co.uk>
2017-10-29 17:53:51 +0000

    added a tip to the javascript course

--- 04_object_prototypes.test.js
+++ 04_object_prototypes.test.js
@@ -2,6 +2,10 @@
 /*
     
     Task 1) Write a function that capitalises a string. To do this, add a prototype function to the base String object.
+
+      Note: You shouldn't do this (unless polyfilling old browsers) as this is modifying the native objects
+
+      Hint: To update a strings prototype use String.prototype.yourFunctionName = 
     
 */ 
 
